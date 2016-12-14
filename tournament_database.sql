396cad22c54e92e52282a33e3788e312cdffee2d
Adam <adamh93@verizon.net>
2016-12-13 21:20:17 -0500

    Added additional comments as well as clarify more test code.

--- tournament_database.sql
+++ tournament_database.sql
@@ -23,15 +23,16 @@ CREATE TABLE matches(
     SELECT EMP_ID, NAME, DEPT FROM COMPANY LEFT OUTER JOIN DEPARTMENT
         ON COMPANY.ID = DEPARTMENT.EMP_ID;
 
-    -- View for the total wins a player has within the tournament
-    CREATE VIEW total_wins  as SELECT players.id as id, count(winner) as wins FROM players, matches WHERE winner = players.id GROUP BY players.id ORDER BY wins DESC;
     -- View for the total wins a person has within the tournament
     CREATE VIEW wins as SELECT players.id, count(winner) as wins FROM players LEFT JOIN matches ON players.id = matches.winner group by players.id;
+
     -- View for the total matches a player has played in the tournament
     CREATE VIEW total_matches  as SELECT players.id as id, count(*) as matches FROM players, matches WHERE players.id in (winner, loser) GROUP BY players.id;
-    '''TEST CODE
-    CREATE VIEW standings as SELECT players.id as pid, total_wins.total_wins as wins, total_matches.total_matches as matches from players, matches where players.id = total_wins.id AND total_wins.id = total_matches.id;
-    '''
 
+    '''TEST CODE
+    Code for standings was not functioning properly so I opted to use the code for play_stands view to look at the standings.
     -- View for the standings for the players within the tournament which utilizes data from total wins view and total matches view to a standings record for each player.
-    CREATE VIEW play_stands as SELECT players.id, players.name, (SELECT count(winner) FROM matches WHERE winner = players.id) as wins, (SELECT count(*) FROM matches WHERE players.id in (winner, loser)) as total_matches FROM players ORDER BY total_wins DESC, total_matches DESC;
+    CREATE VIEW standings as SELECT players.id, players.name, total_wins.total_wins as wins, total_matches.total_matches as matches from players, matches where players.id = total_wins.id AND total_wins.id = total_matches.id;
+    '''
+    -- View for the standings for the players within the tournament which utilizes data from total wins view and total matches view to a standings record for each player.
+    CREATE VIEW play_stands as SELECT players.id, players.name, (SELECT count(winner) FROM matches WHERE winner = players.id) as total_wins, (SELECT count(*) FROM matches WHERE players.id in (winner, loser)) as total_matches FROM players ORDER BY total_wins DESC, total_matches DESC;
